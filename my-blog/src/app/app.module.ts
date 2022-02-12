38e589f0e0f5a29fb815084fdc56261dca0fdb07
Nikita Lezhnev <catassasin331@gmail.com>
2022-02-12 11:56:51 +0300

    Added Articles page

--- my-blog/src/app/app.module.ts
+++ my-blog/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactModule } from './contact/contact.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [
