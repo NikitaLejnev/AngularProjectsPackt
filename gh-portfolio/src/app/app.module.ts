f0d08d9844b7b3df5c7efc7a04c1e599c500f60d
Nikita Lezhnev <catassasin331@gmail.com>
2022-02-18 12:15:15 +0300

    Add TransferHttpCacheModule

--- gh-portfolio/src/app/app.module.ts
+++ gh-portfolio/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { PanelComponent } from './panel/panel.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { OrganizationsComponent } from './organizations/organizations.component';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { OrganizationsComponent } from './organizations/organizations.component'
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    HttpClientModule
+    HttpClientModule,
+    TransferHttpCacheModule
   ],
   providers: [],
   bootstrap: [AppComponent]
