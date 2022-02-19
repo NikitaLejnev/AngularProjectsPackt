e49169bf3ab282a7e47868dbcfa9587b6e7e4e63
Nikita Lezhnev <catassasin331@gmail.com>
2022-02-19 09:41:57 +0300

    Configure the state

--- packt/apps/tour/src/app/app.module.ts
+++ packt/apps/tour/src/app/app.module.ts
@@ -1,22 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule,
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
-      { path: 'admin', loadChildren: () =>
-       import('@packt/admin').then(m => m.AdminModule) },
-      { path: 'tour', loadChildren: () => 
-        import ('@packt/visitor').then(m =>
-          m.VisitorModule) },
-      { path: '', pathMatch: 'full', redirectTo: 'tour' }
-    ])
+      {
+        path: 'admin',
+        loadChildren: () => import('@packt/admin').then((m) => m.AdminModule),
+      },
+      {
+        path: 'tour',
+        loadChildren: () =>
+          import('@packt/visitor').then((m) => m.VisitorModule),
+      },
+      { path: '', pathMatch: 'full', redirectTo: 'tour' },
+    ]),
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ packt/libs/poi/src/lib/poi.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import * as fromPoi from './+state/poi.reducer';
+import { PoiEffects } from './+state/poi.effects';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    StoreModule.forFeature(fromPoi.POI_FEATURE_KEY, fromPoi.reducer),
+    EffectsModule.forFeature([PoiEffects]),
+  ],
+})
+export class PoiModule {}
